a778b6fefeedddd08a86b6a6306b2537bbb5a350
Jeongsang Baek <hackest@gmail.com>
2020-07-22 01:36:35 +0900

    use curl

--- index.js
+++ index.js
@@ -1,40 +1,32 @@
-const axios = require('axios')
 const cheerio = require('cheerio')
 const twilio = require('twilio')
+const util = require('util')
+const exec = util.promisify(require('child_process').exec)
 require('dotenv').config()
 
-const getHtml = async () => {
+exports.doProcess = async () => {
     try {
         const URL = 'https://www.tesla.com/ko_kr/modely'
-        return await axios.get(URL)
-    } catch (err) {
-        console.error(err)
-    }
-}
+        const {stdout, stderr} = await exec(`curl ${URL}`)
+        const $ = cheerio.load(stdout)
+        const domSelector = '#tesla-hero-showcase-172 > div > div.hero-regions > div > div.hero-region--center-bottom > div > div.hero-callouts--button.cmp-animate--to_reveal > a'
+        const urlLink = $(domSelector).attr('href')
+        const expectedUrl = '/ko_kr/updates'
+        console.log(`purchaseBtn: ${urlLink}`)
+        if (urlLink === expectedUrl) {
+            console.log(`Matched to expected URL: ${expectedUrl}`)
+        } else {
+            console.log(`! Tesla URL is not matched to expectedUrl. URL:${urlLink}, Expected: ${expectedUrl}`)
+            //send SMS
+            const accountSid = process.env.ACCOUNT_SID
+            const authToken = process.env.AUTH_TOKEN
+            const fromNumber = process.env.FROM_NUMBER
+            const toNumber = process.env.TO_NUMBER
+            const client = twilio(accountSid, authToken)
+            const message = await client.messages.create({body: 'Tesla starts sell model Y in Korea!', from:fromNumber, to: toNumber})
+            console.log(`Message sent: ${message.sid}`)
+        }
 
-exports.doProcess = () => {
-    try {
-        getHtml()
-        .then( async html => {
-            const $ = cheerio.load(html.data)
-            const domSelector = '#tesla-hero-showcase-172 > div > div.hero-regions > div > div.hero-region--center-bottom > div > div.hero-callouts--button.cmp-animate--to_reveal > a'
-            const urlLink = $(domSelector).attr('href')
-            const expectedUrl = '/ko_kr/updates'
-            console.log(`purchaseBtn: ${urlLink}`)
-            if (urlLink === expectedUrl) {
-                console.log(`Matched to expected URL: ${expectedUrl}`)
-            } else {
-                console.log(`! Tesla URL is not matched to expectedUrl. URL:${urlLink}, Expected: ${expectedUrl}`)
-                //send SMS
-                const accountSid = process.env.ACCOUNT_SID
-                const authToken = process.env.AUTH_TOKEN
-                const fromNumber = process.env.FROM_NUMBER
-                const toNumber = process.env.TO_NUMBER
-                const client = twilio(accountSid, authToken)
-                const message = await client.messages.create({body: 'Tesla starts sell model Y in Korea!', from:fromNumber, to: toNumber})
-                console.log(`Message sent: ${message.sid}`)
-            }
-        })
     } catch (err) {
         console.error(`Err: ${err}`)
     }
